103167e973ea7535703109d657babecd93f75a79
Dan Foad <danielfoad@gmail.com>
2018-01-30 22:32:44 +0000

    Setup react, electron & webpack

---
+++ app/src/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+//import {} from './styles/global.css'
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
