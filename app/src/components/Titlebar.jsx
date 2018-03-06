e290c15523700c8a681686b2f1197ae974500f3e
Dan Foad <danielfoad@gmail.com>
2018-03-06 00:16:31 +0000

    Change maximise icon on click

--- app/src/components/Titlebar.jsx
+++ app/src/components/Titlebar.jsx
@@ -5,16 +5,26 @@ import {remote} from 'electron'
 import {} from '../style/global.scss'
 
 export default class Titlebar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            maximise: 'src/img/window-restore.png'
+        }
+    }
 
     minimise() {
         remote.getCurrentWindow().minimize()
     }
 
     maximise() {
-        if (remote.getCurrentWindow().isMaximized())
+        if (remote.getCurrentWindow().isMaximized()){
             remote.getCurrentWindow().restore()
-        else
-        remote.getCurrentWindow().maximize()
+            this.setState({ maximise: 'src/img/window-maximize.png' })
+        } else {
+            remote.getCurrentWindow().maximize()
+            this.setState({ maximise: 'src/img/window-restore.png' })
+        }
     }
 
     close() {
@@ -25,7 +35,7 @@ export default class Titlebar extends Component {
         return (
             <div className='titlebar'>
                 <div className='titlebar__button close' onClick={ this.close }><img src='src/img/close.png' /></div>
-                <div className='titlebar__button restore' onClick={ this.maximise }><img src='src/img/window-restore.png' /></div>
+                <div className='titlebar__button restore' onClick={ this.maximise }><img src={ this.state.maximise } /></div>
                 <div className='titlebar__button minimize' onClick={ this.minimise }><img src='src/img/window-minimize.png' /></div>
             </div>
         )
