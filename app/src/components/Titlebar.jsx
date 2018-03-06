be88c5be34ec9bdc8c2040984094aa7fb25cfba8
Dan Foad <danielfoad@gmail.com>
2018-03-06 00:08:50 +0000

    Added titlebar actions

--- app/src/components/Titlebar.jsx
+++ app/src/components/Titlebar.jsx
@@ -1,15 +1,32 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import {remote} from 'electron'
 
 import {} from '../style/global.scss'
 
 export default class Titlebar extends Component {
+
+    minimize() {
+        remote.BrowserWindow.getFocusedWindow().minimize()
+    }
+
+    maximise() {
+        if (remote.BrowserWindow.isMaximized())
+            remote.BrowserWindow.getFocusedWindow().restore()
+        else
+        remote.BrowserWindow.getFocusedWindow().maximize()
+    }
+
+    close() {
+        remote.BrowserWindow.getFocusedWindow.close()
+    }
+
     render() {
         return (
             <div className='titlebar'>
-                <div className='titlebar__button close'><img src='src/img/close.png' /></div>
-                <div className='titlebar__button restore'><img src='src/img/window-restore.png' /></div>
-                <div className='titlebar__button minimize'><img src='src/img/window-minimize.png' /></div>
+                <div className='titlebar__button close' onClick={ this.close }><img src='src/img/close.png' /></div>
+                <div className='titlebar__button restore' onClick={ this.maximise }><img src='src/img/window-restore.png' /></div>
+                <div className='titlebar__button minimize' onClick={ this.close }><img src='src/img/window-minimize.png' /></div>
             </div>
         )
     }
